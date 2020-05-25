cc09c4abf27ccf8f2d16e066c63bf4d86bf209f6
Will Harris <will.t.harris@protonmail.com>
2020-05-25 15:21:26 -0700

    feat: add faren to C conversion

--- ch-3/temp_converter/src/temp_converter.rs
+++ ch-3/temp_converter/src/temp_converter.rs
@@ -13,19 +13,36 @@ fn main() {
     if input.trim() == "F" {
         println!("Please input the celcius value you'd like to convert to farenheit:");
 
-        let mut celciusInput = String::new();
+        let mut celcius_input = String::new();
 
         io::stdin()
-            .read_line(&mut celciusInput)
+            .read_line(&mut celcius_input)
             .expect("Error with the celcius input");
 
-        let value = convert_to_farenheit(celciusInput.trim().parse::<f32>().unwrap());
+        let value = convert_to_farenheit(celcius_input.trim().parse::<f32>().unwrap());
 
         println!("That is {} degrees farenheit!", value);
     }
+
+    if input.trim() == "C" {
+        println!("Please input the farenheit value you'd like to convert to celcius:");
+
+        let mut farenheit_input = String::new();
+
+        io::stdin()
+            .read_line(&mut farenheit_input)
+            .expect("Error with the farenheit input");
+
+        let value = convert_to_celcuis(farenheit_input.trim().parse().unwrap());
+
+        println!("That is {} degrees celcius!", value);
+    }
 }
 
-fn convert_to_celcuis() {}
+fn convert_to_celcuis(temp: f32) -> f32 {
+    let converted_temp = (&temp - 32.0) * (5.0 / 9.0);
+    converted_temp
+}
 
 fn convert_to_farenheit(temp: f32) -> f32 {
     let converted_temp = (&temp - 32.0) / 1.8;
