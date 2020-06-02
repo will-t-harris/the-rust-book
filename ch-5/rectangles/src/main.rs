874cb77de0c3bd11b0cedc0a9f9b0accce27a23e
Will Harris <will.t.harris@protonmail.com>
2020-06-02 14:31:19 -0700

    refactor: same area fn using a tuple

--- ch-5/rectangles/src/main.rs
+++ ch-5/rectangles/src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
-    println!(
-        "The are of the rectangle is {} square pixels",
-        area(width1, height1)
-    );
+    println!("The are of the rectangle is {} square pixels", area(rect1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
