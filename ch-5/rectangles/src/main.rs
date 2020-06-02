79a04113a3bb6795c4e18a6c3751f50e54b1a0b3
Will Harris <will.t.harris@protonmail.com>
2020-06-02 14:45:56 -0700

    refactor: add Debug trait to Rectangle struct

--- ch-5/rectangles/src/main.rs
+++ ch-5/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -10,6 +11,7 @@ fn main() {
     };
 
     println!("The are of the rectangle is {} square pixels", area(&rect1));
+    println!("rect1 is {:#?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
