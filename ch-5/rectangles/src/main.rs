7ee20c43f92c33931cef3bf94d15d4d5a8bb754a
Will Harris <will.t.harris@protonmail.com>
2020-06-02 15:12:34 -0700

    feat: define can_hold method with 2 params

--- ch-5/rectangles/src/main.rs
+++ ch-5/rectangles/src/main.rs
@@ -4,16 +4,32 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other_rectangle: &Rectangle) -> bool {
+        self.width > other_rectangle.width && self.height > other_rectangle.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("The are of the rectangle is {} square pixels", area(&rect1));
-    println!("rect1 is {:#?}", rect1);
-}
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.height * rectangle.width
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
